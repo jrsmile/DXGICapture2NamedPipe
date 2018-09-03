cbb5e02fa1b119e7ba5df5340be69ad1218fab58
Jan Reiss <jan@behead.de>
2018-09-03 20:36:22 +0200

    initial repeat, lagging

--- DXGICaptureSample/DXGICaptureSample.cpp
+++ DXGICaptureSample/DXGICaptureSample.cpp
@@ -3,15 +3,12 @@
 
 #include "stdafx.h"
 #include "DXGIManager.h"
+#include <windows.h>
 
 DXGIManager g_DXGIManager;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("DXGICaptureSample. Fast windows screen capture\n");
-	printf("Capturing desktop to: capture.bmp\n");
-	printf("Log: logfile.log\n");
-
 	CoInitialize(NULL);
 
 	g_DXGIManager.SetCaptureSource(CSMonitor1);
@@ -26,12 +23,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	DWORD dwBufSize = dwWidth*dwHeight*4;
 
 	BYTE* pBuf = new BYTE[dwBufSize];
-
-	CComPtr<IWICImagingFactory> spWICFactory = NULL;
-	HRESULT hr = spWICFactory.CoCreateInstance(CLSID_WICImagingFactory);
-	if( FAILED(hr) )
-		return hr;
-
+	HRESULT hr;
+	
 	int i=0;
 	do
 	{
@@ -46,61 +39,29 @@ int _tmain(int argc, _TCHAR* argv[])
 		return hr;
 	}
 
-	printf("Saving capture to file\n");
+	UINT tmp;
+	UINT offset = dwWidth * 4 * 5;
+	tmp = ((pBuf[offset+ 1]) << 24) | ((pBuf[offset + 2]) << 16) | ((pBuf[offset + 3]) << 8) | ((pBuf[offset + 4]));
+	printf("first pixel: %u\n",tmp);
+	
+	while (true) {
 
-	CComPtr<IWICBitmap> spBitmap = NULL;
-	hr = spWICFactory->CreateBitmapFromMemory(dwWidth, dwHeight, GUID_WICPixelFormat32bppBGRA, dwWidth*4, dwBufSize, (BYTE*)pBuf, &spBitmap);
-	if( FAILED(hr) )
-		return hr;
-		
-	CComPtr<IWICStream> spStream = NULL;
+		i = 0;
+		do
+		{
+			hr = g_DXGIManager.GetOutputBits(pBuf, rcDim);
+			i++;
+		} while (hr == DXGI_ERROR_WAIT_TIMEOUT || i < 2);
 
-	hr = spWICFactory->CreateStream(&spStream);
-	if (SUCCEEDED(hr)) {
-		hr = spStream->InitializeFromFilename(L"capture.bmp", GENERIC_WRITE);
-	}
+		if (FAILED(hr))
+		{
+			printf("GetOutputBits failed with hr=0x%08x\n", hr);
+			return hr;
+		}
 
-	CComPtr<IWICBitmapEncoder> spEncoder = NULL;
-	if (SUCCEEDED(hr)) {
-		hr = spWICFactory->CreateEncoder(GUID_ContainerFormatBmp, NULL, &spEncoder);
-	}
-
-	if (SUCCEEDED(hr)) {
-		hr = spEncoder->Initialize(spStream,WICBitmapEncoderNoCache);
-	}
-
-	CComPtr<IWICBitmapFrameEncode> spFrame = NULL;
-	if (SUCCEEDED(hr)) {
-		hr = spEncoder->CreateNewFrame(&spFrame, NULL);
-	}
-
-	if (SUCCEEDED(hr)) {
-		hr = spFrame->Initialize(NULL);
-	}
-
-	if (SUCCEEDED(hr)) {
-		//hr = spFrame->SetSize(dwWidth, dwHeight);
-		hr = spFrame->SetSize(360, 18);
-
-	}
-
-	WICPixelFormatGUID format;
-	spBitmap->GetPixelFormat(&format);
-
-	if (SUCCEEDED(hr)) {
-		hr = spFrame->SetPixelFormat(&format);
-	}
-
-	if (SUCCEEDED(hr)) {
-		hr = spFrame->WriteSource(spBitmap, NULL);
-	}
-		
-	if (SUCCEEDED(hr)) {
-		hr = spFrame->Commit();
-	}
-
-	if (SUCCEEDED(hr)) {
-		hr = spEncoder->Commit();
+		tmp = ((pBuf[offset + 1]) << 24) | ((pBuf[offset + 2]) << 16) | ((pBuf[offset + 3]) << 8) | ((pBuf[offset + 4]));
+		printf("pixel: %u\n", tmp);
+		Sleep(100);
 	}
 
 	delete[] pBuf;
