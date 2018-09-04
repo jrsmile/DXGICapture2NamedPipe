d449a94fe41e50c0f199b89ec23a1641f6fa4923
Jan Reiss <jan@behead.de>
2018-09-04 18:53:11 +0200

    first working example

--- DXGICaptureSample/DXGICaptureSample.cpp
+++ DXGICaptureSample/DXGICaptureSample.cpp
@@ -40,32 +40,16 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 
 	UINT tmp;
-	UINT offset = dwWidth * 4 * 5;
-	tmp = ((pBuf[offset+ 1]) << 24) | ((pBuf[offset + 2]) << 16) | ((pBuf[offset + 3]) << 8) | ((pBuf[offset + 4]));
-	printf("first pixel: %u\n",tmp);
-	
-	while (true) {
-
-		i = 0;
-		do
-		{
-			hr = g_DXGIManager.GetOutputBits(pBuf, rcDim);
-			i++;
-		} while (hr == DXGI_ERROR_WAIT_TIMEOUT || i < 2);
-
-		if (FAILED(hr))
-		{
-			printf("GetOutputBits failed with hr=0x%08x\n", hr);
-			return hr;
-		}
-
+	UINT offset;
+	for (int a = 0; a < 360; a = a + 4) {
+		offset = a * 4;
 		tmp = ((pBuf[offset + 1]) << 24) | ((pBuf[offset + 2]) << 16) | ((pBuf[offset + 3]) << 8) | ((pBuf[offset + 4]));
-		printf("pixel: %u\n", tmp);
-		Sleep(100);
+		if (tmp != 65280){
+			printf("%u pixel: %u\n", a, tmp);
 	}
-
+	}
+	
 	delete[] pBuf;
 
 	return 0;
 }
-
